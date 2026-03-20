dac0324039768e0450b9e807992d3b6df4cf57a9
haaaaaaatan <n80080518@gmail.com>
2026-03-20 13:19:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`BOT 起動完了：${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "/ping") {
+    message.reply("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
+
 
